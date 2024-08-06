b6cd91900006aca300c4794e46729f62434ceaf3
Vladislav <holastoun@gmai.com>
2024-08-06 12:19:39 +0500

    add header

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
+import { MantineProvider } from '@mantine/core';
+import { HeaderMegaMenu } from './components/header/HeaderMegaMenu';
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<MantineProvider>
+<HeaderMegaMenu/>
+  </MantineProvider>,
 )
